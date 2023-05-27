4550b4b2e3875f950caa2d3464e2619d16a2710d
lolik-1 <131038970+lolik-1@users.noreply.github.com>
2023-05-27 19:46:09 +0500

    Update Icons.jsx

--- src/Components/Icons.jsx
+++ src/Components/Icons.jsx
@@ -1,8 +1,10 @@
 function Icons ({ImgUrl}){
     return(
         <ul className="flex gap-10"> 
-            <li><img src={ImgUrl}></img></li>
-            
+            <li><img src="./img/Vk.svg"></img></li>
+            <li><img src="./img/You.svg"></img></li>
+            <li><img src="./img/Tg.svg"></img></li>
+            <li><img src="./img/Chat.svg"></img></li>
         </ul> 
     )
 }
